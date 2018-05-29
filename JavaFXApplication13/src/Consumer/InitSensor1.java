bac00cc948e60500555a1abc81c64a0882ea4c13
Chr0n0s <39704796+Titan008@users.noreply.github.com>
2018-05-29 14:03:32 +0200

    Add files via upload

--- JavaFXApplication13/src/Consumer/InitSensor1.java
+++ JavaFXApplication13/src/Consumer/InitSensor1.java
@@ -1,24 +1,25 @@
 package Consumer;
 
-
-import java.util.ArrayList;
-
 import de.hft.wiinf.cebarround.CeBarRoundDataSensor;
 import de.hft.wiinf.cebarround.CeBarRoundObserver;
 import de.hft.wiinf.cebarround.SensorEvent;
 import de.hft.wiinf.cebarround.SensorRegister;
 
-final class InitSensor1 {
+public class InitSensor1 {
 
 	SensorRegister app = new CeBarRoundDataSensor();
-	 public ArrayList <SensorEvent> events = new ArrayList <>();
 
 	public void listen() {
-		final CeBarRoundObserver<SensorEvent> listener = currentevent -> 
-		events.add(currentevent);
-
+		final CeBarRoundObserver<SensorEvent> listener = event -> {
+			event.getTemperature();
+			event.getPressure();
+			event.getRevolutions();
+			event.getDate();
+			event.getSensorTypeCode();
+			event.getUniqueSensorIdentifier();
+		};
+		
 		app.addListener(listener);
-
 	}
 
 	public void startMeasure() {
@@ -36,4 +37,5 @@ final class InitSensor1 {
 	public void getVerbosity() {
 		System.out.println(app.getVebosity());
 	}
+
 }
